df514c1c7ccd6d7babd64260bcf0431f6a7f3b63
Adrian Stevens <adrian.stevens@hotmail.com>
2015-02-03 22:39:50 -0800

    BLE Dashboard - Added features to the BLEService and included the Android Support Libraries

--- Android/BLEDashboard/app/src/main/java/com/beetroot/bledashboard/BLEService.java
+++ Android/BLEDashboard/app/src/main/java/com/beetroot/bledashboard/BLEService.java
@@ -2,11 +2,20 @@ package com.beetroot.bledashboard;
 
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
+import android.support.v4.content.LocalBroadcastManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Adrian on 2/2/2015.
@@ -15,10 +24,20 @@ public class BLEService extends Service {
     // Binder given to clients
     private final IBinder mBinder = new BLEServiceBinder();
 
-    private BluetoothManager mBTManager;
-    private BluetoothAdapter mBTAdapter;
-    private BluetoothGatt mBTGatt;
-    private String mBTAddress;
+    //these values are specific to the Grove BLE V1 - update if you're using a different module
+    private static String GROVE_SERVICE = "0000ffe0-0000-1000-8000-00805f9b34fb";
+    private static String CHARACTERISTIC_TX = "0000ffe1-0000-1000-8000-00805f9b34fb";
+    private static String CHARACTERISTIC_RX = "0000ffe1-0000-1000-8000-00805f9b34fb";
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final long SCAN_PERIOD = 5000; //5 seconds
+    private static final String DEVICE_NAME = "HMSoft"; //display name for Grove BLE
+
+    private BluetoothAdapter mBluetoothAdapter;//our local adapter
+    private BluetoothGatt mBluetoothGatt; //provides the GATT functionality for communication
+    private BluetoothGattService mBluetoothGattService; //service on mBlueoothGatt
+    private static List<BluetoothDevice> mDevices = new ArrayList<BluetoothDevice>();//discovered devices in range
+    private BluetoothDevice mDevice; //external BLE device (Grove BLE module)
 
     /**
      * Class used for the client Binder.  Because we know this service always
@@ -38,10 +57,58 @@ public class BLEService extends Service {
 
     @Override
     public boolean onUnbind(Intent intent) {
-        if (mBTGatt != null) {
-            mBTGatt.close();
-            mBTGatt = null;
+        if (mBluetoothGatt != null) {
+            mBluetoothGatt.close();
+            mBluetoothGatt = null;
         }
         return super.onUnbind(intent);
     }
+
+    // Send an Intent with an action named "custom-event-name". The Intent
+    // sent should
+    // be received by the ReceiverActivity.
+    private void sendBroadcast(final String message) {
+        Log.d("BLE", "sendBroadcast: " + message);
+
+        Intent intent = new Intent(message);
+
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
+    //output helper method
+    private void statusUpdate (final String msg) {
+        Log.w("BLE", msg);
+    }
+
+    private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                statusUpdate("Connected");
+                statusUpdate("Searching for services");
+                mBluetoothGatt.discoverServices();
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                statusUpdate("Device disconnected");
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                List<BluetoothGattService> gattServices = mBluetoothGatt.getServices();
+
+                for(BluetoothGattService gattService : gattServices) {
+                    statusUpdate("Service discovered: " + gattService.getUuid());
+                    if(GROVE_SERVICE.equals(gattService.getUuid().toString()))
+                    {
+                        mBluetoothGattService = gattService;
+                        statusUpdate("Found communication Service");
+                        //sendMessage();
+                    }
+                }
+            } else {
+                statusUpdate("onServicesDiscovered received: " + status);
+            }
+        }
+    };
 }
